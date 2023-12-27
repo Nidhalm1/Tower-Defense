4e4248696f15440e1993acb496c0f3c5c652a81e
Jules Peris <jules.peris@etu.u-paris.fr>
2023-12-27 20:25:35 +0100

    Ajout de fonction InRange pour Tower (verifie si toure peut defendre sur monstre)

--- src/main/Tower.java
+++ src/main/Tower.java
@@ -17,4 +17,10 @@ public class Tower extends Entity{
         this.cost = cost;
         this.attackDistance = attackDistance;
     }
+
+    public boolean inRange(Monster monster){
+        return (monster.getX() - getX() )<= attackDistance && (monster.getX() - getX() ) > 0;
+        // regarde si la distance entre le monstre et la tour est bien compris dans la distance d'attaque
+        // si la distance est négative cela veut dire que le monstre est derrière la toure elle ne peut donc pas defendre dessus
+    }
 }
